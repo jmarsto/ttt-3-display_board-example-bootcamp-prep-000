baee476558ff05fb0a32f38d62b3b096fc72ea91
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-04-07 17:34:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "a tic tac toe board"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
